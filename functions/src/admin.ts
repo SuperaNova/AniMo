2b94735f17ee62a1acbc2bc246fa0f64f34fa285
jermochi <jrvnryle@gmail.com>
2025-05-15 23:36:56 +0800

    removed functions folder

--- functions/src/admin.ts
+++
@@ -1,9 +0,0 @@
-import {initializeApp} from "firebase-admin/app";
-import {getFirestore} from "firebase-admin/firestore";
-import * as admin from "firebase-admin"; // Import the full admin namespace
-
-initializeApp();
-
-const db = getFirestore();
-
-export {db, admin};
