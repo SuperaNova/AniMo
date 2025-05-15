a93d54b97c4c0aabfefe1c0ed6eb4e17910127e9
jewican <james.ewican@cit.edu>
2025-05-15 22:53:42 +0800

    api key change

--- lib/core/widgets/auth_wrapper.dart
+++ lib/core/widgets/auth_wrapper.dart
@@ -1,11 +1,9 @@
-import 'package:animo/features/farmer/screens/farmer_dashboard_overhaul.dart';
 import 'package:animo/features/farmer/screens/farmer_main.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:animo/core/models/app_user.dart'; // Your AppUser model
 import 'package:animo/services/firebase_auth_service.dart'; // Your AuthService
 import 'package:animo/features/auth/screens/landing_screen.dart';
-import 'package:animo/features/farmer/screens/farmer_dashboard_screen.dart';
 import 'package:animo/features/buyer/screens/buyer_dashboard_screen.dart';
 import 'package:animo/features/driver/screens/driver_dashboard_screen.dart';
 
